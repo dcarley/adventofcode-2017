172444f692064bfb1979247d9f1fe6aad89a9797
Dan Carley <dan.carley@gmail.com>
2017-12-03 22:36:08 +0000

    d02p2: Solution.

--- day02/day02.go
+++ day02/day02.go
@@ -7,47 +7,72 @@ import (
 )
 
 func Part1(input []byte) (int, error) {
+	checksumFunc := func(nums []int) int {
+		var low, high *int
+
+		for i, num := range nums {
+			if low == nil || num < *low {
+				low = &nums[i]
+			}
+			if high == nil || num > *high {
+				high = &nums[i]
+			}
+		}
+
+		return *high - *low
+	}
+
+	return bothParts(input, checksumFunc)
+}
+
+func Part2(input []byte) (int, error) {
+	checksumFunc := func(nums []int) int {
+		for x, _ := range nums {
+			for y, _ := range nums {
+				if x == y {
+					continue
+				}
+
+				if nums[x]%nums[y] == 0 {
+					return nums[x] / nums[y]
+				}
+			}
+		}
+
+		return 0
+	}
+
+	return bothParts(input, checksumFunc)
+}
+
+func bothParts(input []byte, checksumFunc func([]int) int) (int, error) {
 	var checksum int
 
-	lines := bufio.NewScanner(bytes.NewBuffer(input))
-	for lines.Scan() {
-		sum, err := ChecksumLine(lines.Bytes())
-		if err != nil {
+	tableScanner := bufio.NewScanner(bytes.NewBuffer(input))
+	for tableScanner.Scan() {
+		lineScanner := bufio.NewScanner(bytes.NewBuffer(tableScanner.Bytes()))
+		lineScanner.Split(bufio.ScanWords)
+
+		var nums []int
+		for lineScanner.Scan() {
+			num, err := strconv.Atoi(lineScanner.Text())
+			if err != nil {
+				return 0, err
+			}
+
+			nums = append(nums, num)
+		}
+
+		if err := lineScanner.Err(); err != nil {
 			return 0, err
 		}
 
-		checksum += sum
+		checksum += checksumFunc(nums)
 	}
 
-	if err := lines.Err(); err != nil {
+	if err := tableScanner.Err(); err != nil {
 		return 0, err
 	}
 
 	return checksum, nil
-}
-
-func ChecksumLine(line []byte) (int, error) {
-	var low, high *int
-
-	fields := bufio.NewScanner(bytes.NewBuffer(line))
-	fields.Split(bufio.ScanWords)
-	for fields.Scan() {
-		num, err := strconv.Atoi(fields.Text())
-		if err != nil {
-			return 0, err
-		}
-
-		if low == nil || num < *low {
-			low = &num
-		}
-		if high == nil || num > *high {
-			high = &num
-		}
-	}
-
-	if err := fields.Err(); err != nil {
-		return 0, err
-	}
-
-	return *high - *low, nil
 }
--- day02/day02_test.go
+++ day02/day02_test.go
@@ -12,15 +12,15 @@ import (
 
 var _ = Describe("Day02", func() {
 	Describe("Part1", func() {
-		DescribeTable("Checksum",
+		DescribeTable("example lines",
 			func(input []byte, expected int) {
-				checksum, err := ChecksumLine(input)
+				checksum, err := Part1(input)
 				Expect(err).ToNot(HaveOccurred())
 				Expect(expected).To(Equal(checksum))
 			},
-			Entry("example 5 1 9 5", []byte("5 1 9 5"), 8),
-			Entry("example 7 5 3", []byte("7 5 3"), 4),
-			Entry("example 2 4 6 8", []byte("2 4 6 8"), 6),
+			Entry("5 1 9 5", []byte("5 1 9 5"), 8),
+			Entry("7 5 3", []byte("7 5 3"), 4),
+			Entry("2 4 6 8", []byte("2 4 6 8"), 6),
 		)
 
 		It("should solve example", func() {
@@ -39,6 +39,37 @@ var _ = Describe("Day02", func() {
 			checksum, err := Part1(input)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(checksum).To(Equal(43074))
+		})
+	})
+
+	Describe("Part2", func() {
+		DescribeTable("example lines",
+			func(input []byte, expected int) {
+				checksum, err := Part2(input)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(checksum).To(Equal(expected))
+			},
+			Entry("5 9 2 8", []byte("5 9 2 8"), 4),
+			Entry("9 4 7 3", []byte("9 4 7 3"), 3),
+			Entry("3 8 6 5", []byte("3 8 6 5"), 2),
+		)
+
+		It("should solve example", func() {
+			input := []byte(`5 9 2 8
+9 4 7 3
+3 8 6 5`)
+			checksum, err := Part2(input)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(checksum).To(Equal(9))
+		})
+
+		It("should solve puzzle input", func() {
+			input, err := ioutil.ReadFile("day02.input")
+			Expect(err).ToNot(HaveOccurred())
+
+			checksum, err := Part2(input)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(checksum).To(Equal(280))
 		})
 	})
 })
