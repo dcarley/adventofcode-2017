7f923d141507243a6a6a891ad269d62652cce99e
Dan Carley <dan.carley@gmail.com>
2017-12-07 15:33:58 +0000

    d03p2: Solution.

--- day03/day03.go
+++ day03/day03.go
@@ -48,6 +48,45 @@ func Part1(targetValue int) int {
 	return int(math.Abs(float64(position.X)) + math.Abs(float64(position.Y)))
 }
 
+func Part2(targetAfterValue int) int {
+	var (
+		side, sideLength int
+		cellValue        int
+		position         Position
+	)
+
+	// pre-populate first cell because it has no neighbours yet
+	cells := map[Position]int{
+		position: 1,
+	}
+
+	for cellValue <= targetAfterValue {
+		side++
+
+		// the length increments every other side
+		if side%2 == 0 {
+			sideLength++
+		}
+
+		bearing := side % 4
+		for moves := 0; moves < sideLength && cellValue <= targetAfterValue; moves++ {
+			position = Move(position, bearing, 1)
+
+			// sum neighbours
+			cellValue = 0
+			for _, neighbour := range Neighbours(position) {
+				if val, ok := cells[neighbour]; ok {
+					cellValue += val
+				}
+			}
+
+			cells[position] = cellValue
+		}
+	}
+
+	return cellValue
+}
+
 func Move(position Position, bearing, moves int) Position {
 	switch bearing {
 	case BearNorth:
@@ -61,4 +100,25 @@ func Move(position Position, bearing, moves int) Position {
 	}
 
 	return position
+}
+
+func Neighbours(position Position) []Position {
+	neighbours := []int{-1, 0, 1}
+	positions := make([]Position, 0, len(neighbours)^2-1)
+
+	for _, xOffset := range neighbours {
+		for _, yOffset := range neighbours {
+			// ignore centre (self)
+			if xOffset == 0 && yOffset == 0 {
+				continue
+			}
+
+			positions = append(positions, Position{
+				X: position.X + xOffset,
+				Y: position.Y + yOffset,
+			})
+		}
+	}
+
+	return positions
 }
--- day03/day03_test.go
+++ day03/day03_test.go
@@ -9,18 +9,39 @@ import (
 )
 
 var _ = Describe("Day03", func() {
-	Describe("Part1", func() {
-		DescribeTable("Move",
-			func(bearing int, expected Position) {
-				position := Move(Position{0, 0}, bearing, 3)
-				Expect(position).To(Equal(expected))
-			},
-			Entry("north", BearNorth, Position{0, -3}),
-			Entry("east", BearEast, Position{3, 0}),
-			Entry("south", BearSouth, Position{0, 3}),
-			Entry("west", BearWest, Position{-3, 0}),
-		)
+	DescribeTable("Move",
+		func(bearing int, expected Position) {
+			position := Move(Position{0, 0}, bearing, 3)
+			Expect(position).To(Equal(expected))
+		},
+		Entry("north", BearNorth, Position{0, -3}),
+		Entry("east", BearEast, Position{3, 0}),
+		Entry("south", BearSouth, Position{0, 3}),
+		Entry("west", BearWest, Position{-3, 0}),
+	)
 
+	DescribeTable("Neighbours",
+		func(position Position, expected []Position) {
+			Expect(Neighbours(position)).To(Equal(expected))
+		},
+		Entry("0,0", Position{0, 0}, []Position{
+			{-1, -1}, {-1, 0}, {-1, 1},
+			{0, -1}, {0, 1},
+			{1, -1}, {1, 0}, {1, 1},
+		}),
+		Entry("2,2", Position{2, 2}, []Position{
+			{1, 1}, {1, 2}, {1, 3},
+			{2, 1}, {2, 3},
+			{3, 1}, {3, 2}, {3, 3},
+		}),
+		Entry("-2,-2", Position{-2, -2}, []Position{
+			{-3, -3}, {-3, -2}, {-3, -1},
+			{-2, -3}, {-2, -1},
+			{-1, -3}, {-1, -2}, {-1, -1},
+		}),
+	)
+
+	Describe("Part1", func() {
 		DescribeTable("examples",
 			func(input int, expected int) {
 				Expect(Part1(input)).To(Equal(expected))
@@ -33,6 +54,25 @@ var _ = Describe("Day03", func() {
 
 		It("should solve puzzle input", func() {
 			Expect(Part1(277678)).To(Equal(475))
+		})
+	})
+
+	Describe("Part2", func() {
+		DescribeTable("examples",
+			func(input int, expected int) {
+				Expect(Part2(input)).To(Equal(expected))
+			},
+			Entry("value after 2", 2, 4),
+			Entry("value after 4", 4, 5),
+			Entry("value after 5", 5, 10),
+			Entry("value after 10", 10, 11),
+			Entry("value after 11", 11, 23),
+			Entry("value after 23", 23, 25),
+			Entry("value after 25", 25, 26),
+		)
+
+		It("should solve puzzle input", func() {
+			Expect(Part2(277678)).To(Equal(279138))
 		})
 	})
 })
