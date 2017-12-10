0eae59556149d2d1f31c9cce756f2daa85924260
Dan Carley <dan.carley@gmail.com>
2017-12-10 22:39:08 +0000

    d07p2: Solution
    Could be more efficient using an actual graph.

--- day07/day07.go
+++ day07/day07.go
@@ -3,38 +3,125 @@ package day07
 import (
 	"bufio"
 	"io"
+	"strconv"
 	"strings"
 )
 
-func Part1(input io.Reader) (string, error) {
-	nodes := map[string]string{}
+type Node struct {
+	Parent string
+	Weight int
+}
+
+type Nodes map[string]Node
+
+func Parse(input io.Reader) (Nodes, string, error) {
+	nodes := Nodes{}
 
 	scanner := bufio.NewScanner(input)
 	for scanner.Scan() {
 		line := strings.SplitN(scanner.Text(), " ", 4)
 		name := line[0]
-		if _, exists := nodes[name]; !exists {
-			nodes[name] = ""
+
+		weight, err := strconv.Atoi(strings.Trim(line[1], "()"))
+		if err != nil {
+			return Nodes{}, "", err
+		}
+
+		if node, exists := nodes[name]; exists {
+			node.Weight = weight
+			nodes[name] = node
+		} else {
+			nodes[name] = Node{
+				Weight: weight,
+			}
 		}
 
 		if len(line) > 2 {
 			for _, child := range strings.Split(line[3], ", ") {
-				nodes[child] = name
+				if node, exists := nodes[child]; exists {
+					node.Parent = name
+					nodes[child] = node
+				} else {
+					nodes[child] = Node{
+						Parent: name,
+					}
+				}
 			}
 		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		return "", err
-	}
-
 	var root string
-	for name, parent := range nodes {
-		if parent == "" {
+	for name, node := range nodes {
+		if node.Parent == "" {
 			root = name
 			break
 		}
 	}
 
+	return nodes, root, scanner.Err()
+}
+
+type UnbalancedNode struct {
+	Name   string
+	Weight int
+}
+
+func TraverseChildren(nodes Nodes, parent string) (int, *UnbalancedNode) {
+	branchWeights := map[int][]string{}
+
+	for name, node := range nodes {
+		if node.Parent == parent {
+			branchWeight, unbalanced := TraverseChildren(nodes, name)
+			if unbalanced != nil {
+				return 0, unbalanced
+			}
+
+			branchWeight += node.Weight
+			branchWeights[branchWeight] = append(branchWeights[branchWeight], name)
+		}
+	}
+
+	var sum, correctWeight, incorrectWeight int
+
+	for weight, names := range branchWeights {
+		sum += weight * len(names)
+
+		if len(names) == 1 {
+			incorrectWeight = weight
+		} else {
+			correctWeight = weight
+		}
+	}
+
+	if incorrectWeight != 0 {
+		name := branchWeights[incorrectWeight][0]
+		correction := incorrectWeight - correctWeight
+
+		return 0, &UnbalancedNode{
+			Name:   name,
+			Weight: nodes[name].Weight - correction,
+		}
+	}
+
+	return sum, nil
+}
+
+func Part1(input io.Reader) (string, error) {
+	_, root, err := Parse(input)
+	if err != nil {
+		return "", err
+	}
+
 	return root, nil
+}
+
+func Part2(input io.Reader) (string, int, error) {
+	nodes, root, err := Parse(input)
+	if err != nil {
+		return "", 0, err
+	}
+
+	_, unbalanced := TraverseChildren(nodes, root)
+
+	return unbalanced.Name, unbalanced.Weight, nil
 }
--- day07/day07_test.go
+++ day07/day07_test.go
@@ -2,6 +2,7 @@ package day07_test
 
 import (
 	"bytes"
+	"io"
 	"os"
 
 	. "github.com/dcarley/adventofcode-2017/day07"
@@ -11,9 +12,10 @@ import (
 )
 
 var _ = Describe("Day07", func() {
-	Describe("Part1", func() {
-		It("should solve example", func() {
-			input := bytes.NewBuffer([]byte(`pbga (66)
+	var example io.Reader
+
+	BeforeEach(func() {
+		example = bytes.NewBuffer([]byte(`pbga (66)
 xhth (57)
 ebii (61)
 havc (66)
@@ -26,8 +28,11 @@ jptl (61)
 ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)`))
+	})
 
-			root, err := Part1(input)
+	Describe("Part1", func() {
+		It("should solve example", func() {
+			root, err := Part1(example)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(root).To(Equal("tknk"))
 		})
@@ -40,6 +45,26 @@ cntj (57)`))
 			root, err := Part1(file)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(root).To(Equal("wiapj"))
+		})
+	})
+
+	Describe("Part2", func() {
+		It("should solve example", func() {
+			node, weight, err := Part2(example)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(node).To(Equal("ugml"))
+			Expect(weight).To(Equal(60))
+		})
+
+		It("should solve puzzle input", func() {
+			file, err := os.Open("day07.input")
+			Expect(err).ToNot(HaveOccurred())
+			defer file.Close()
+
+			node, weight, err := Part2(file)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(node).To(Equal("eionkb"))
+			Expect(weight).To(Equal(1072))
 		})
 	})
 })
